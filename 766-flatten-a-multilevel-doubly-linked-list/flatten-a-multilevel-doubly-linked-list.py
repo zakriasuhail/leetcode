530e14c67f9fb25f475f089f0f3cf9d6cc7fa0ab
zakriasuhail <73486892+zakriasuhail@users.noreply.github.com>
2025-02-22 19:31:48 -0800

    Time: 46 ms (20.93%) | Memory: 18.1 MB (92.31%) - LeetSync

--- 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.py
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.py
@@ -1,74 +1,36 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val, prev, next, child):
-        self.val = val
-        self.prev = prev
-        self.next = next
-        self.child = child
-
-
-1 -> 2 -> 5 -> 6
-     3 -> 4 -> x
-==================
-
-1 <-> 2 <-> 3 <-> 4 <-> 5 <-> 6
-     
-
-    flat(1)
-    curr = 3
-    childNode = 3
-    nextNode = 5
-    tailNode = 4
-
-        flat(3)
-        curr = 4
-
-    1
-    2
-    3
-
-"""
-
 class Solution:
     def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
-
-        if not head:
+        if not head: 
             return None
 
-        curr = head
-        while curr:
-
-            # if we find child node
-            if curr.child:
-                nextNode = curr.next
+        def flat(head):
+            curr = head
+            prev = None
+            while curr:
                 
-                # find tail of child list
-                tail = curr.child
-                while tail.next:
-                    tail = tail.next
+                # process sublist
+                if curr.child:
+                    childNode = curr.child
+                    nextNode = curr.next
+                    
+                    # rewire current node
+                    curr.next = childNode
+                    childNode.prev = curr
+                    
+                    # get and rewire tail
+                    tailNode = flat(childNode)
+                    if tailNode and nextNode:
+                        tailNode.next = nextNode
+                        nextNode.prev = tailNode
+
+                    # set child to None
+                    curr.child = None
+
+                # update iterators
+                prev = curr
+                curr = curr.next
+            return prev
             
-                # rewire
-                curr.next = curr.child
-                curr.child.prev = curr
-                tail.next = nextNode
-                if nextNode:
-                    nextNode.prev = tail
-
-                curr.child = None
-
-            curr = curr.next
+        flat(head)
         return head
-                
-
-
-
-
-
-
-
-
-
-
-
         
