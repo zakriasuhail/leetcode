c9b00fc7dcc6cc985294948e68bf8b740f5742b6
zakriasuhail <73486892+zakriasuhail@users.noreply.github.com>
2025-02-23 17:40:35 -0800

    Time: 1 ms (44.17%) | Memory: 17.8 MB (99.20%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -17,17 +17,21 @@ class Solution:
             
             # base case
             if openBracs + closeBracs == n * 2:
-                res.append(path)
+                res.append("".join(path))
                 return
 
             # recursive
             if openBracs > closeBracs:
-                dfs(path + ")", openBracs, closeBracs + 1)
+                path.append(")")
+                dfs(path, openBracs, closeBracs + 1)
+                path.pop()
             if openBracs < n:
-                dfs(path + "(", openBracs + 1, closeBracs)
+                path.append("(")
+                dfs(path, openBracs + 1, closeBracs)
+                path.pop()
 
 
-        dfs("(", 1, 0)
+        dfs(["("], 1, 0)
         return res
 
         
