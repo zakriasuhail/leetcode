62c5e0daaf7c53d1fbb20cbc4e1844b5642f5daa
zakriasuhail <73486892+zakriasuhail@users.noreply.github.com>
2025-02-23 21:12:03 -0800

    Time: 7 ms (72.87%) | Memory: 21.7 MB (14.63%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.py
@@ -0,0 +1,27 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        sortedIntervals = sorted(intervals, key=lambda x:x[0])
+        res = [sortedIntervals[0]]
+        for start, end in sortedIntervals[1:]:
+            prevStart, prevEnd = res[-1]
+
+            # case 1: partial overlap
+            if start <= prevEnd and end >= prevEnd:
+                res[-1][1] = max(end, prevEnd)
+
+
+            # case 2: full overlap
+            elif start < prevEnd and end < prevEnd:
+                continue
+
+            # case 3: no overlap
+            else:
+                res.append([start, end])
+        return res
+
+            
+
+
+
+
+        
