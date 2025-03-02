589f8204a3dbef7d497e62a096df75d54a3eff50
Zakria <73486892+zakriasuhail@users.noreply.github.com>
2025-03-02 12:11:50 -0800

    Time: 263 ms (40.98%) | Memory: 20.2 MB (69.20%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -6,7 +6,6 @@ class Solution:
             if (row + x >= 0 and row + x < len(grid) and
                 col + y >= 0 and col + y < len(grid[0]) and 
                 grid[row + x][col + y] == "1"):
-                grid[row + x][col + y] = "0"
                 neighbors.append((row + x, col + y))
         return neighbors
 
@@ -18,13 +17,11 @@ class Solution:
             queue = deque([node])
             while queue:
                 row, col = queue.popleft()
-
-                # mark as visited
-                grid[row][col] = "0"
                 
                 # get neighbors
-                for neighbor in self.getNeighbors(row, col, grid):
-                    queue.append(neighbor)
+                for nr, nc in self.getNeighbors(row, col, grid):
+                    grid[nr][nc] = "0"
+                    queue.append((nr, nc))
 
 
         
@@ -33,6 +30,7 @@ class Solution:
             for col in range(len(grid[0])): 
                 if grid[row][col] == "1":
                     islands += 1
+                    grid[row][col] = "0"
                     bfs((row, col))
         return islands
 
