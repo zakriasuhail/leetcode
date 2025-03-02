fd50e8fecac513833c7baa8fa63e6bfd3085591a
Zakria <73486892+zakriasuhail@users.noreply.github.com>
2025-03-02 12:12:38 -0800

    Time: 268 ms (36.19%) | Memory: 20 MB (82.88%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -9,19 +9,20 @@ class Solution:
                 neighbors.append((row + x, col + y))
         return neighbors
 
+    def bfs(self, node, grid):
+        queue = deque([node])
+        while queue:
+            row, col = queue.popleft()
+            
+            # get neighbors
+            for nr, nc in self.getNeighbors(row, col, grid):
+                grid[nr][nc] = "0"
+                queue.append((nr, nc))
+
 
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        # run breadth first search
-        def bfs(node):
-            queue = deque([node])
-            while queue:
-                row, col = queue.popleft()
-                
-                # get neighbors
-                for nr, nc in self.getNeighbors(row, col, grid):
-                    grid[nr][nc] = "0"
-                    queue.append((nr, nc))
+
 
 
         
@@ -31,7 +32,7 @@ class Solution:
                 if grid[row][col] == "1":
                     islands += 1
                     grid[row][col] = "0"
-                    bfs((row, col))
+                    self.bfs((row, col), grid)
         return islands
 
 
