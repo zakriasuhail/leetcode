06fabb0a76a0a243cf1c4dc4a9c8292ce654cc45
Zakria <73486892+zakriasuhail@users.noreply.github.com>
2025-03-02 22:31:36 -0800

    Time: 0 ms (100.00%) | Memory: 17.6 MB (84.08%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -20,38 +20,33 @@ class Solution:
 
         i = 0
         while i < len(s):
+
             ch = s[i]
 
-            # if we reach a number
-            if ch.isdigit():
-                
-                # get number
-                j = i
-                while s[j].isdigit():
-                    j += 1
-                
-                num = int(s[i:j])
+            if ch == "]":
 
-                stack.append(num)
-                i = j - 1
-
-            elif ch == "]":
+                # get string
                 string = ""
                 while stack[-1] != "[":
                     string = stack.pop() + string
-                
-                stack.pop()
-                num = stack.pop()
-                stack.append(string * num)
 
+                # pop off opening brac
+                stack.pop()
+
+                # build number
+                num = ""
+                while stack and stack[-1].isdigit():
+                    num = stack.pop() + num
+
+                # append string back
+                stack.append(int(num) * string)
+            
             else:
                 stack.append(ch)
 
             i += 1
         return "".join(stack)
 
-
-            
 
 
 
