ad328e3089b154255f4d76e2bc2e1deaa8ded91c
Zakria <73486892+zakriasuhail@users.noreply.github.com>
2025-03-19 19:07:01 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (65.22%) - LeetSync

---
+++ 62-unique-paths/unique-paths.py
@@ -0,0 +1,14 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        matrix = [[1] * n] * m
+
+        for i in range(1, m):
+            for j in range(1, n):
+                
+                matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1]
+        return matrix[-1][-1]
+
+        
+            
+
+        
