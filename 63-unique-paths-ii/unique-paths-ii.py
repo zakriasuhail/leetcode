45407b3c2a79e9e208d47ae9761004cca0a9fc74
Zakria <73486892+zakriasuhail@users.noreply.github.com>
2025-03-22 22:38:39 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (91.61%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -13,10 +13,9 @@
 """
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        
         rows = len(obstacleGrid)
         cols = len(obstacleGrid[0])
-
-        dp = [[0] * cols for row in range(rows)] 
 
         # edge case
         if obstacleGrid[0][0] or obstacleGrid[-1][-1]:
@@ -26,19 +25,19 @@ class Solution:
             for col in range(cols):
                 # if obstacle
                 if obstacleGrid[row][col]:
-                    dp[row][col] = 0
+                    obstacleGrid[row][col] = 0
                 
                 # if space
                 else:
 
                     if row - 1 >= 0 and col - 1 >= 0:
-                        dp[row][col] = dp[row - 1][col] + dp[row][col - 1]
+                        obstacleGrid[row][col] = obstacleGrid[row - 1][col] + obstacleGrid[row][col - 1]
                     elif row - 1 >= 0:
-                        dp[row][col] = dp[row - 1][col]
+                        obstacleGrid[row][col] = obstacleGrid[row - 1][col]
                     elif col - 1 >= 0:
-                        dp[row][col] = dp[row][col - 1]
+                        obstacleGrid[row][col] = obstacleGrid[row][col - 1]
                     else:
-                        dp[row][col] = 1
-        return dp[-1][-1]
+                        obstacleGrid[row][col] = 1
+        return obstacleGrid[-1][-1]
                     
 
