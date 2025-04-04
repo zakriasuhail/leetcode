ba46e2dea867fd08d15196aee49c81fd10c95072
Zakria <73486892+zakriasuhail@users.noreply.github.com>
2025-04-04 11:37:18 -0700

    Time: 330 ms (96.50%) | Memory: 17.8 MB (97.14%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,48 +1,35 @@
 """
 
-  a b c d e
-a 1 0 0 0 0
-c 0 0 1 0 0
-e 0 0 0 0 1 
+  a c e 0
+a 3     0
+b   2 . 0
+c   2 1 0
+d   . 1 0
+e   . 1 0
+  0 0 0 0
 
-[0, 0, 0, 0, 0, 0]
-[0, 1, 1, 1, 1, 0]
-[0, 0, 0, 0, 0, 0] 
-[0, 0, 0, 0, 0, 0]
-
-rows = 4
-cols = 6
-
-row = 2
-col = 4
 
 """
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        
+
         if len(text2) < len(text1):
             text1, text2 = text2, text1
+
         
-        # create dp arrays
         curr = [0] * (len(text1) + 1)
         prev = [0] * (len(text1) + 1)
 
-        # iterate
-        for row in range(len(text2) - 1, -1, -1):
-            for col in range(len(text1) - 1, -1, -1):
-
-                # if we have a match
-                if text1[col] == text2[row]:
-                    curr[col] = 1 + prev[col + 1]
-                else:
-                    curr[col] = max(curr[col + 1], prev[col])
+        for i in range(len(text2) - 1, -1, -1):
+            for j in range(len(text1) -1, -1, -1):
                 
-            # swap at each row
+                if text1[j] == text2[i]:
+                    curr[j] = 1 + prev[j + 1]
+                else:
+                    curr[j] = max(curr[j + 1], prev[j])
+
             curr, prev = prev, curr
         return prev[0]
-
-                
-
 
 
 
