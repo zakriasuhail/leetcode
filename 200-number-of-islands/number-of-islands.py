b370d587045378844c68dc2424258eaf4ee40835
Zakria <73486892+zakriasuhail@users.noreply.github.com>
2025-04-10 00:43:48 -0700

    Time: 250 ms (55.61%) | Memory: 20.3 MB (44.14%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,50 +1,45 @@
 class Solution:
-    def getNeighbors(self, row, col, grid):
-        directions = [(1,0), (-1,0), (0,1), (0,-1)]
-        neighbors = []
-        for x, y in directions:
-            if (row + x >= 0 and row + x < len(grid) and
-                col + y >= 0 and col + y < len(grid[0]) and 
-                grid[row + x][col + y] == "1"):
-                neighbors.append((row + x, col + y))
-        return neighbors
+    def numIslands(self, grid: List[List[str]]) -> int:
 
-    def bfs(self, node, grid):
-        queue = deque([node])
-        while queue:
-            row, col = queue.popleft()
-            
-            # get neighbors
-            for nr, nc in self.getNeighbors(row, col, grid):
-                grid[nr][nc] = "0"
-                queue.append((nr, nc))
+        def getNeighbors(row, col):
+            neighbors = []
+            directions = [(1,0), (-1,0), (0,1), (0,-1)]
+            for dirRow, dirCol in directions:  
 
-        
-    def dfs(self, row, col, grid):
-        if (row < 0 or row >= len(grid) or
-            col < 0 or col >= len(grid[0]) or 
-            grid[row][col] == "0"):
-            return
-        
-        # mark as visited
-        grid[row][col] = "0"
-
-        # traverse
-        self.dfs(row + 1, col, grid)
-        self.dfs(row - 1, col, grid)
-        self.dfs(row, col + 1, grid)
-        self.dfs(row, col - 1, grid)
-        
+                newRow, newCol = dirRow + row, dirCol + col
 
 
-    def numIslands(self, grid: List[List[str]]) -> int:  
+                if (newRow >= 0 and newRow < rows and
+                    newCol >= 0 and newCol < cols and 
+                    grid[newRow][newCol] == "1"):
+                    neighbors.append((newRow, newCol))
+            return neighbors
+
+        def bfs(coord):
+            queue = deque([coord])
+
+            while queue:
+
+                row, col = queue.popleft()
+
+                # mark as visited
+                grid[row][col] = "0"
+
+                for nr, nc in getNeighbors(row, col):
+                    grid[nr][nc] = "0"
+                    queue.append((nr, nc))
+
+
+
+        rows = len(grid)
+        cols = len(grid[0])
         islands = 0
-        for row in range(len(grid)):
-            for col in range(len(grid[0])): 
+        for row in range(rows):
+            for col in range(cols):
                 if grid[row][col] == "1":
                     islands += 1
-                    self.dfs(row, col, grid)
-        return islands
+                    bfs((row, col))
 
+        return islands
 
         
