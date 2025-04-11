532437eb0c39ff92fbef23e081319d136bf61eee
Zakria <73486892+zakriasuhail@users.noreply.github.com>
2025-04-11 14:14:15 -0700

    Time: 56 ms (33.03%) | Memory: 20.6 MB (8.28%) - LeetSync

--- 382-linked-list-random-node/linked-list-random-node.py
+++ 382-linked-list-random-node/linked-list-random-node.py
@@ -6,19 +6,25 @@
 class Solution:
 
     def __init__(self, head: Optional[ListNode]):
-        self.nodes = 0
-        self.map = {}
-
-        curr = head
-        while curr:
-            self.map[self.nodes] = curr.val
-            self.nodes += 1
-            curr = curr.next
-
+        self.curr = head
         
 
     def getRandom(self) -> int:
-        return self.map[random.randint(0, self.nodes - 1)]
+        node = self.curr
+        count = 0
+        pick = -1
+
+        while node:
+
+            count += 1
+
+            if random.randint(1, count) == count:
+                pick = node.val
+
+            node = node.next
+        return pick
+
+
         
 
 
